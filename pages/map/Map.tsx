32aedd191bf2ae9191a19f016efc87e1a19711ed
aguchhait09 <aguchhait1999@gmail.com>
2024-02-12 16:33:20 +0530

    df

--- pages/map/Map.tsx
+++ pages/map/Map.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { MutableRefObject, Ref, useCallback, useEffect, useMemo, useRef, useState } from 'react'
 import { MapContainer, Marker, Popup, TileLayer, useMapEvents } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import { endpoints } from '@/api/endpoints';
@@ -6,13 +6,13 @@ import { AllNetwork } from '@/typescript/interface/allNetwork.interface';
 import { useQuery } from '@tanstack/react-query';
 import { axiosInstance } from '@/api/axiosInstance';
 import assets from '@/json/assets';
-import { Icon } from 'leaflet';
+import { Icon, LatLngBounds, Map } from 'leaflet';
 import { FullscreenControl } from 'react-leaflet-fullscreen';
 import "react-leaflet-fullscreen/styles.css";
 import ReactLeafletGoogleLayer from "react-leaflet-google-layer";
 import useGeolocation from '@/hooks/useGeolocation';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { Box, Button } from '@mui/material';
+import { Box, Button, Grid, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
 import Control from 'react-leaflet-custom-control';
 
 interface PropsType {
@@ -20,7 +20,7 @@ interface PropsType {
   long: number;
 }
 
-const Map = () => {
+const Mapp = () => {
 
   const { isPending: loading, error: err, data: bikepoints } = useQuery({
     queryKey: ['bike'],
@@ -38,92 +38,160 @@ const Map = () => {
     iconSize: [32, 32]
   })
 
+  console.log('bikepoints', bikepoints)
+
   const location = useGeolocation()
+  console.log('loc', location);
 
-  const [switched, setSwitched] = useState("")
 
+  const [switched, setSwitched] = useState("");
+  const [mapBounds, setMapBounds] = useState<LatLngBounds | null>(null)
+
+
+  const map = useRef<Map | null>(null)
+
+  const onMapIniti = useCallback((mapInstance: Map | null) => {
+    mapInstance?.addEventListener("dragend", () => {
+      setMapBounds(mapInstance?.getBounds());
+    });
+  }, [])
+
+  console.log(map.current, "vv")
+
+
+  const withinBoundsData = useMemo(() => {
+    if (!mapBounds) {
+      return []
+    };
+    return bikepoints?.filter((item) => {
+      if (!item?.location?.latitude || !item?.location?.longitude) {
+        return false
+      }
+      return mapBounds?.intersects(new LatLngBounds([[item?.location?.latitude, item?.location?.longitude]]))
+    })
+  }, [mapBounds])
+console.log("withinBoundsData",withinBoundsData)
 
   return (
     <>
-      <MapContainer
-        center={[0, 0]}
-        zoom={2}
-        scrollWheelZoom={true}
-        style={{
-          height: "100vh",
-          backgroundRepeat: 'no-repeat'
-        }}
-      >
+      <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+        <Grid item xs={6}>
+          <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>Company</TableCell>
+                  <TableCell >City</TableCell>
+                  <TableCell >Country</TableCell>
+                  <TableCell >Name</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {
+                  withinBoundsData?.map((item) => {
+                    return (
+                      <>
+                        <TableRow
+                          sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                        >
+                          <TableCell component="th" scope="row">{item?.company}
+                          </TableCell>
+                          <TableCell ></TableCell>
+                          <TableCell ></TableCell>
+                          <TableCell ></TableCell>
+                        </TableRow>
+                      </>
+                    )
+                  })
+                }
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Grid>
+        <Grid item xs={6}>
+          <MapContainer
+            center={[0, 0]}
+            zoom={2}
 
-        {
-          switched === 'satelite' ? (
-            <>
-              <TileLayer
-                key="stretview"
-                attribution="Tiles &copy; Avik"
-                url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
-              />
-            </>
-          ) : (
-            <>
-              <TileLayer
-                key="mapview"
-                attribution='&copy; Avik'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-              />
-            </>
-          )
-        }
-        {
-          bikepoints?.map((item, key) => {
-            return (
-              <>
-                <Marker
-                  icon={myIcon}
-                  key={key} position={[item?.location?.latitude, item?.location?.longitude]}>
-                  <Popup>
-                    Name: {item?.name} <br /> City: {item?.location?.city} <br /> Country: {item?.location?.country} <br /> Company: {item?.company}
-                  </Popup>
-                </Marker>
-              </>
-            )
-          })
-        }
-        <FullscreenControl />
-        <Control prepend position='topright'>
-          {
-            switched === 'satelite' ? (
-              <>
-                <Box>
-                  <Button sx={{
-                    backgroundColor: 'white', color: 'black', '&:hover': {
-                      backgroundColor: 'white',
-                      boxShadow: 'none',
-                    }
-                  }} onClick={() => setSwitched('normal')}>
-                    Street View
-                  </Button>
-                </Box>
-              </>
-            ) : (
-              <>
-                <Box sx={{ my: 1 }}>
-                  <Button sx={{
-                    backgroundColor: '#333634', color: 'white', '&:hover': {
-                      backgroundColor: '#333634',
-                      boxShadow: 'none',
-                    }
-                  }} onClick={() => setSwitched('satelite')}>
-                    Satelite View
-                  </Button>
-                </Box>
-              </>
-            )
-          }
-        </Control>
-      </MapContainer>
+            scrollWheelZoom={true}
+            style={{
+              height: "100vh",
+              backgroundRepeat: 'no-repeat'
+            }}
+            ref={onMapIniti}
+          >
+            {
+              switched === 'satelite' ? (
+                <>
+                  <TileLayer
+                    key="stretview"
+                    attribution="Tiles &copy; Avik"
+                    url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+                  />
+                </>
+              ) : (
+                <>
+                  <TileLayer
+                    key="mapview"
+                    attribution='&copy; Avik'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                  />
+                </>
+              )
+            }
+            {
+              bikepoints?.map((item, key) => {
+                return (
+                  <>
+                    <Marker
+                      icon={myIcon}
+                      key={key} position={[item?.location?.latitude, item?.location?.longitude]}>
+                      <Popup>
+                        Name: {item?.name} <br /> City: {item?.location?.city} <br /> Country: {item?.location?.country} <br /> Company: {item?.company}
+                      </Popup>
+                    </Marker>
+                  </>
+                )
+              })
+            }
+            <FullscreenControl />
+            <Control prepend position='topright'>
+              {
+                switched === 'satelite' ? (
+                  <>
+                    <Box>
+                      <Button sx={{
+                        backgroundColor: 'white', color: 'black', '&:hover': {
+                          backgroundColor: 'white',
+                          boxShadow: 'none',
+                        }
+                      }} onClick={() => setSwitched('normal')}>
+                        Street View
+                      </Button>
+                    </Box>
+                  </>
+                ) : (
+                  <>
+                    <Box sx={{ my: 1 }}>
+                      <Button sx={{
+                        backgroundColor: '#333634', color: 'white', '&:hover': {
+                          backgroundColor: '#333634',
+                          boxShadow: 'none',
+                        }
+                      }} onClick={() => setSwitched('satelite')}>
+                        Satelite View
+                      </Button>
+                    </Box>
+                    
+                  </>
+                )
+              }
+            </Control>
+          </MapContainer>
+        </Grid>
+      </Grid>
     </>
   )
 }
-export default Map
+export default Mapp
 
